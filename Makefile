dcde970f84c60c27fde127584d5ee2be4a5a7cdf
_ <_@_>
2017-07-22 18:29:56 -0400

    intermediate buffers expand when needed

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 
 all: pfa
 
-realtime: realtime.c
-	gcc -Wall -ansi -pedantic -std=c89 -fno-omit-frame-pointer -fsanitize=address -Os pfa.c -o pfa 
+pfa: pfa.c
+	gcc -Wall -ansi -pedantic -std=c99 -fno-omit-frame-pointer -Os pfa.c -o pfa 
 
 clean:
 	rm *.o pfa
--- pfa.c
+++ pfa.c
@@ -2,12 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* The maximum unwrapped line length. */
-#define BUFSIZE 4096
-
-/* Cast characters to the simplest equivalent character. Thus A -> a, 4 -> 0 */
-char classification_table[256];
-
 /* Tokens: things which can't be split.
  * For instance,
  *   TOK_LABEL includes everything from 'import' to 'quit' to 'try'
@@ -54,6 +48,38 @@ enum {
   LINE_IS_TRISTR,
   LINE_IS_NORMAL,
 };
+
+struct vlbuf {
+  union {
+    void *vd;
+    char *ch;
+    int *in;
+  } d;
+  size_t len;
+  size_t esize;
+};
+
+struct vlbuf vlbuf_make(size_t es) {
+  struct vlbuf ib;
+  ib.esize = es;
+  ib.len = 16;
+  ib.d.vd = malloc(ib.len * ib.esize);
+  return ib;
+}
+
+size_t vlbuf_expand(struct vlbuf *ib, size_t minsize) {
+  do {
+    ib->len *= 2;
+  } while (ib->len <= minsize);
+  ib->d.vd = realloc(ib->d.vd, ib->len * ib->esize);
+  return ib->len;
+}
+
+void vlbuf_free(struct vlbuf *ib) {
+  free(ib->d.vd);
+  ib->d.vd = 0;
+  ib->len = 0;
+}
 
 const char *tok_to_string(int tok) {
   switch (tok) {
@@ -143,11 +169,16 @@ const char *specnames[] = {"if",  "then",  "else",    "import", "except",
                            "as",  "else",  "finally", NULL};
 
 void pyformat(FILE *file, FILE *out) {
-  char linebuf[BUFSIZE];
-  const char *lbufend = &linebuf[BUFSIZE - 1];
+  struct vlbuf linebuf = vlbuf_make(sizeof(char));
+  struct vlbuf tokbuf = vlbuf_make(sizeof(char));
+  struct vlbuf toks = vlbuf_make(sizeof(int));
+  struct vlbuf lsp = vlbuf_make(sizeof(char));
+  struct vlbuf laccum = vlbuf_make(sizeof(char));
+  struct vlbuf splitpoints = vlbuf_make(sizeof(int));
+  struct vlbuf split_ratings = vlbuf_make(sizeof(int));
+  struct vlbuf split_nestings = vlbuf_make(sizeof(int));
+  struct vlbuf lineout = vlbuf_make(sizeof(char));
 
-  char tokbuf[BUFSIZE];
-  int toks[BUFSIZE];
   char *tokd = NULL;
   char *stokd = NULL;
   int ntoks = 0;
@@ -155,13 +186,28 @@ void pyformat(FILE *file, FILE *out) {
   char string_starter = '\0';
   int line_state = LINE_IS_NORMAL;
   int leading_spaces = 0;
-  int ltok = TOK_INBETWEEN;
   int neof = 0;
   int nestings = 0;
+  int netlen = 0;
   while (1) {
-    char *readct = fgets(linebuf, lbufend - linebuf, file);
+    char *readct;
+    int llen = 0;
+    while (1) {
+      readct = fgets(&linebuf.d.ch[llen], linebuf.len - 3 - llen, file);
+      if (!readct)
+        break;
+      int rlen = strlen(readct);
+      llen += rlen;
+      if (linebuf.d.ch[llen - 1] != '\n') {
+        vlbuf_expand(&linebuf, llen + 3);
+      } else {
+        break;
+      }
+    }
+
     if (!readct) {
-      if (line_state == LINE_IS_NORMAL || line_state == LINE_IS_BLANK || neof) {
+      if ((line_state == LINE_IS_NORMAL || line_state == LINE_IS_BLANK) ||
+          neof) {
         break;
       } else {
         neof = 1;
@@ -169,19 +215,36 @@ void pyformat(FILE *file, FILE *out) {
     }
 
     if (line_state == LINE_IS_NORMAL || line_state == LINE_IS_BLANK) {
+      netlen = llen;
       ntoks = 0;
-      tokd = tokbuf;
-      stokd = tokbuf;
+      /* Ensure buffers can hold the worst case line */
+      if (tokbuf.len < netlen * 2) {
+        vlbuf_expand(&tokbuf, netlen * 2);
+        vlbuf_expand(&toks, netlen * 2);
+      }
+      tokd = tokbuf.d.ch;
+      stokd = tokbuf.d.ch;
       nestings = 0;
+    } else {
+      /* Adjust buffers in the case of line extension */
+      netlen += llen;
+      int tokdoff = tokd - tokbuf.d.ch;
+      int stokdoff = stokd - tokbuf.d.ch;
+      if (tokbuf.len < netlen * 2) {
+        vlbuf_expand(&tokbuf, netlen * 2);
+        vlbuf_expand(&toks, netlen * 2);
+      }
+      tokd = &tokbuf.d.ch[tokdoff];
+      stokd = &tokbuf.d.ch[stokdoff];
     }
 
     /* token-split the line with NULL characters; double NULL is eof */
 
     /* STATE MACHINE TOKENIZE */
-    char *cur = linebuf;
-    int llen = readct ? strlen(cur) : 0;
+    char *cur = linebuf.d.ch;
     /* space char gives room for termination checks */
-    cur[llen - 1] = '\n';
+    if (llen > 0)
+      cur[llen - 1] = '\n';
     cur[llen] = '\0';
     cur[llen + 1] = '\0';
 
@@ -205,9 +268,6 @@ void pyformat(FILE *file, FILE *out) {
     } else if (line_state == LINE_IS_BLANK) {
       line_state = LINE_IS_NORMAL;
     }
-
-    //    fprintf(out, "###%s|%d|%s", ls_to_string(line_state), is_whitespace,
-    //    cur);
 
     if (line_state == LINE_IS_NORMAL) {
       leading_spaces = 0;
@@ -385,7 +445,7 @@ void pyformat(FILE *file, FILE *out) {
         if (nxt == '#') {
           proctok = TOK_COMMENT;
           /* nix the terminating newline */
-          linebuf[llen - 1] = ' ';
+          linebuf.d.ch[llen - 1] = ' ';
         } else if (nxt == '"' || nxt == '\'') {
           string_starter = nxt;
           proctok = TOK_STRING;
@@ -448,7 +508,7 @@ void pyformat(FILE *file, FILE *out) {
         } else if (otok == TOK_CBRACE) {
           nestings--;
         }
-        toks[ntoks] = otok;
+        toks.d.in[ntoks] = otok;
         stokd = tokd;
         ntoks++;
       }
@@ -456,12 +516,12 @@ void pyformat(FILE *file, FILE *out) {
     *tokd = '\0';
 
     /* determine if the next line shall continue this one */
-    ltok = toks[ntoks - 1];
     if (line_state == LINE_IS_BLANK) {
       line_state = LINE_IS_BLANK;
     } else if (proctok == TOK_TRISTR) {
       line_state = LINE_IS_TRISTR;
-    } else if (ltok == TOK_LCONT || nestings > 0) {
+    } else if ((ntoks > 0 && toks.d.in[ntoks - 1] == TOK_LCONT) ||
+               nestings > 0) {
       line_state = LINE_IS_CONTINUATION;
     } else {
       line_state = LINE_IS_NORMAL;
@@ -471,31 +531,33 @@ void pyformat(FILE *file, FILE *out) {
       fprintf(out, "\n");
     } else if (line_state == LINE_IS_NORMAL || neof || dumprest) {
       /* Introduce spaces to list */
-      char lsp[BUFSIZE];
-      memset(lsp, ' ', leading_spaces);
-      lsp[leading_spaces] = '\0';
-      char laccum[BUFSIZE];
-      int splitpoints[BUFSIZE];
-      int split_ratings[BUFSIZE]; /* 0 is regular; -1 is force/cmt; 1 is weak */
-      int split_nestings[BUFSIZE];
+
+      /* split ratings 0 is regular; -1 is force/cmt; 1 is weak */
+      if (lsp.len < leading_spaces + 1)
+        vlbuf_expand(&lsp, leading_spaces + 1);
+      memset(lsp.d.vd, ' ', leading_spaces);
+      lsp.d.ch[leading_spaces] = '\0';
+
+      if (laccum.len < 2 * netlen) {
+        vlbuf_expand(&laccum, 2 * netlen);
+        vlbuf_expand(&splitpoints, 2 * netlen);
+        vlbuf_expand(&split_ratings, 2 * netlen);
+        vlbuf_expand(&split_nestings, 2 * netlen);
+        vlbuf_expand(&lineout, 2 * netlen);
+        vlbuf_expand(&laccum, 2 * netlen);
+      }
+
       int nsplits = 0;
-      char *buildpt = laccum;
-      //      fprintf(stderr, ">>%s\n", linebuf);
-      //      fprintf(stderr, "<<%s%s\n", lsp, tokbuf);
-      //      fprintf(stderr, "%d", lbufstart - linebuf);
-      //      for (int i = 0; i < ntoks; i++) {
-      //        fprintf(stderr, " %s", tok_to_string(toks[i]));
-      //      }
-      //      fprintf(stderr, "\n");
+      char *buildpt = laccum.d.ch;
 
       /* Line wrapping & printing, oh joy */
-      char *tokpos = tokbuf;
+      char *tokpos = tokbuf.d.ch;
       //      buildpt += sprintf(buildpt, "%s", lsp);
       int nests = 0;
       for (int i = 0; i < ntoks; i++) {
-        int pptok = i > 0 ? toks[i - 1] : TOK_INBETWEEN;
-        int pretok = toks[i];
-        int postok = toks[i + 1];
+        int pptok = i > 0 ? toks.d.in[i - 1] : TOK_INBETWEEN;
+        int pretok = toks.d.in[i];
+        int postok = toks.d.in[i + 1];
         int toklen = strlen(tokpos);
 
         if (pretok == TOK_LCONT) {
@@ -515,15 +577,15 @@ void pyformat(FILE *file, FILE *out) {
           } else {
             buildpt += sprintf(buildpt, "# %s !#", sos);
           }
-          splitpoints[nsplits] = buildpt - laccum;
-          split_ratings[nsplits] = -1;
-          split_nestings[nsplits] = nests;
+          splitpoints.d.in[nsplits] = buildpt - laccum.d.ch;
+          split_ratings.d.in[nsplits] = -1;
+          split_nestings.d.in[nsplits] = nests;
           nsplits++;
         } else {
           buildpt += sprintf(buildpt, "%s", tokpos);
-          splitpoints[nsplits] = buildpt - laccum;
-          split_ratings[nsplits] = 0;
-          split_nestings[nsplits] = nests;
+          splitpoints.d.in[nsplits] = buildpt - laccum.d.ch;
+          split_ratings.d.in[nsplits] = 0;
+          split_nestings.d.in[nsplits] = nests;
           nsplits++;
         }
         tokpos += toklen + 1;
@@ -596,51 +658,51 @@ void pyformat(FILE *file, FILE *out) {
           nests--;
         }
       }
-      int eoff = buildpt - laccum;
+      int eoff = buildpt - laccum.d.ch;
 
       /* the art of line breaking */
 
       /* TODO: create a 'nesting depth' field */
-      char tmp[BUFSIZE];
+
       int length_left = 80 - leading_spaces;
       int first = 1;
-      fprintf(out, "%s", lsp);
+      fprintf(out, "%s", lsp.d.ch);
       if (nsplits > 0) {
         for (int i = 0; i < nsplits; i++) {
-          int fr = i > 0 ? splitpoints[i - 1] : 0;
-          int to = i >= nsplits - 1 ? eoff : splitpoints[i];
-          memcpy(tmp, &laccum[fr], to - fr);
-          tmp[to - fr] = '\0';
-          if (split_ratings[i] < 0) {
+          int fr = i > 0 ? splitpoints.d.in[i - 1] : 0;
+          int to = i >= nsplits - 1 ? eoff : splitpoints.d.in[i];
+          memcpy(lineout.d.ch, &laccum.d.ch[fr], to - fr);
+          lineout.d.ch[to - fr] = '\0';
+          if (split_ratings.d.in[i] < 0) {
             to -= 2;
-            tmp[to - fr] = '\0';
+            lineout.d.ch[to - fr] = '\0';
           }
           int nlen = to - fr;
-          int force_split = i > 0 ? split_ratings[i - 1] < 0 : 0;
+          int force_split = i > 0 ? split_ratings.d.in[i - 1] < 0 : 0;
 
           if ((nlen < length_left || first) && !force_split) {
             first = 0;
             length_left -= nlen;
-            fprintf(out, "%s", tmp);
+            fprintf(out, "%s", lineout.d.ch);
           } else {
-            char *prn = &tmp[0];
-            if (tmp[0] == ' ') {
-              prn = &tmp[1];
+            char *prn = &lineout.d.ch[0];
+            if (lineout.d.ch[0] == ' ') {
+              prn = &lineout.d.ch[1];
               nlen -= 1;
             }
-            if (force_split || split_nestings[i - 1] > 0) {
+            if (force_split || split_nestings.d.in[i - 1] > 0) {
               fprintf(out, "\n    ");
             } else {
               fprintf(out, " \\\n    ");
             }
             length_left = 80 - leading_spaces - 4 - nlen;
-            fprintf(out, "%s%s", lsp, prn);
+            fprintf(out, "%s%s", lsp.d.ch, prn);
             first = 1;
           }
         }
         fprintf(out, "\n");
       } else {
-        fprintf(out, "%s\n", laccum);
+        fprintf(out, "%s\n", laccum.d.ch);
       }
       if (line_state == LINE_IS_BLANK) {
         fprintf(out, "\n");
@@ -649,6 +711,16 @@ void pyformat(FILE *file, FILE *out) {
       }
     }
   }
+
+  vlbuf_free(&linebuf);
+  vlbuf_free(&tokbuf);
+  vlbuf_free(&toks);
+  vlbuf_free(&lsp);
+  vlbuf_free(&laccum);
+  vlbuf_free(&splitpoints);
+  vlbuf_free(&split_ratings);
+  vlbuf_free(&split_nestings);
+  vlbuf_free(&lineout);
 }
 
 int main(int argc, char **argv) {
